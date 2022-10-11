58ac32c9c3c92271f8a3da24d34bbbf168b7e0f9
Frederik Sthen Hansen <38318697+FrederikSthenHansen@users.noreply.github.com>
2022-10-11 14:48:56 +0200

    Update to make multithreading work and added first instructions in readme

--- PDF-Downloader_Frederik.ps1
+++ PDF-Downloader_Frederik.ps1
@@ -11,24 +11,24 @@
 $myExcelPath= "C:\Users\KOM\Documents\FSH Specialist Academy 2022\Programmeringcase2\opgavefiler/GRI_2017_2020 (1)"
 
 
-#input name of the sheet within the workbook in the code below. Make sure that the input value is within " "
+#input name of the sheet within the workbook, that data has to be gathered from, in the code below. Make sure that the input value is within " "
 # example: $mySheet="0"
 
 $mySheet= "0"
 
 
 #input the path you want the downloaded files deposited at:
-#example: $myOutputPath= "C:\Users\KOM\Documents
+#example: $myOutputPath= "C:\Users\Documents
 
 $myOutputPath= "C:\Users\KOM\Documents\FSH Specialist Academy 2022\Programmeringcase2\Ny mappe (2)\SpecialisternePDFDownloader\Output-Reports\"
 
 #HUSK AT LAVE EN README TIL STUDENTERMEDHJÆLPERE!!!!#
 
 #input start index
-$myStartIndex= 
+#$myStartIndex= 
 
 #Input ending index
-$myEndIndex=
+#$myEndIndex=
 
 # This ends the User Input Area
 ####################################################
@@ -68,47 +68,80 @@ $ExcelWorkSheet = $ExcelWorkBook.Sheets.Item($mySheet)
 $rowsToLoopThrough=( $ExcelWorkSheet.UsedRange.Rows.Count )
 # Write-Output -InputObject $rowsToLoopThrough
 
-
+$myJobs = New-Object System.Collections.ArrayList
  
 $myLoopIterator=1
 while($myLoopIterator-le($rowsToLoopThrough))
 {
- $myName=$ExcelWorkSheet.cells.Item($myLoopIterator, 1).value2
- $myUrl=$ExcelWorkSheet.cells.Item($myLoopIterator, 38).value2
 
- $myDestination="{0}{1}{2}" -f $myOutputPath,$myName,".PDF"
+
+#Br Number
+ $myName=$ExcelWorkSheet.cells.Item($myLoopIterator, 1).value2;
+ 
+
+ #col AL
+ $myUrl=$ExcelWorkSheet.cells.Item($myLoopIterator, 38).value2;
+
+ $myDestination="{0}{1}{2}" -f $myOutputPath,$myName,".PDF";
+ Write-Output -inputObject $myDestination;
+ Write-Output -InputObject $myUrl;
+  
+
 # Write-Output -InputObject $myDestination
         # start downloading PDF
-        $connectionAttempts=0
-        $transferWait=0
+       # $connectionAttempts=0
+       # $transferWait=0
+
+        
  $Job = Start-BitsTransfer -Source $myUrl -Destination $myDestination -DisplayName $myName -Asynchronous 
+
+
+
+
+
  
  # set max download time to 10 seconds and gives it 60 seconds (the minimum allowed by the BitsJob) to succesfully connect
- $Job= Set-BitsTransfer -BitsJob $Job -MaxDownloadTime 20 -RetryInterval 120 -RetryTimeout 60
+ $Job= Set-BitsTransfer -BitsJob $Job -MaxDownloadTime 10 -RetryInterval 60 -RetryTimeout 60
+ # Get-BitsTransfer -AllUsers
 
-    while (($Job.JobState -eq "Transferring") -or ($Job.JobState -eq "Connecting")) `
+ 
+   $myJobs.Add($job)>$null;
+
+    $myLoopIterator++
+}
+
+foreach ($myItem in $myJobs)
+{ 
+
+### this loops for every bit transfer
+while (($myItem.JobState -eq "Transferring") -or ($myItem.JobState -eq "Connecting")) `
        {$transferMessage= "{0} {1}" -f $Job.JobState,$myName;
        Write-Output -InputObject $transferMessage;
-       sleep 5;
-       if($connectionAttempts>3){$Job.re}
-       if ($Job.JobState -eq "Connecting"){$connectionAttempts++}
-       } # Poll for status, sleep for 5 seconds, or perform an action.
+       sleep 3;
+       if($connectionAttempts>3){Write-Output -inputObject "Connection timed out!"; $myItem.State="Error"}
 
-    Switch($Job.JobState)
+       if ($myItem.JobState -eq "Connecting"){$connectionAttempts++;$transferMessage="{0} {1} {2}"-f $transferMessage,"Connection attempts:",$connectionAttempts }
+
+       if($connectionAttempts>3){$myItem.JobState="Error"}
+       } # Poll for status, sleep for 3 seconds, or perform an action.
+
+    Switch($myItem.JobState)
     {    #succesful download
-    "Transferred" { Write-Output -InputObject $transferMessage
-    Complete-BitsTransfer -BitsJob $Job}
+    "Transferred" {$transferMessage="{0} {1}"-f $myName, "is now fully downloaded!"
+     Write-Output -InputObject $transferMessage
+    Complete-BitsTransfer -BitsJob $myItem}
 
     
     #failed
-    "Error" {Write-output -inputObject $transfermesage;  $Job | Format-List } # List the errors.
+    "Error" {Write-output -inputObject $transfermesage;  $myItem | Format-List } # List the errors.
     default {"Other action"} #  Perform corrective action.
 
     #HUSK AT LAVE EN README TIL STUDENTERMEDHJÆLPERE!!!!#
 
     }
 
-    $myLoopIterator++
 }
+
+
 $doneMessage= "All documents attempted downloaded"
 Write-out -inputObject $doneMessage
--- Readme.md
+++ Readme.md
@@ -1,1 +1,12 @@
 
+Important: 
+1.This currently only works in Powershell, when run as Admin, with Developer settings enabled in Windows settings!
+2. Its a good idea to enable viewing "hidden" files in the Pathfinder (stifinder). Its a checkbox under "View" ("vis") in the top bar of the Pathfinder.
+   This is because the script may leave .tmp files of any failed download attempts 
+
+How the script works:
+
+1. The script uses the values from the "User Input Area" section to open the correct excel worksbook and sheet within that workbook.
+2. The script then starts a Bits-transfer Job (download job) for every single row that has been filled in the document. (These take the form of hidden .tmp files)
+3. After every Bits-transfer Job has been started it loops through them all starting at the top, 
+   to determine if the outcome of the download attempt,and creates PDF's of the succesful downloads.
